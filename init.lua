532b3e74c8c8cd8abd4f42b866762669eab3ea0e
oTheAnalyst <michaelvcumbo21@proton.me>
2025-08-28 11:58:29 -0400

    removed some trash

--- init.lua
+++ init.lua
@@ -1,10 +1,1 @@
 require("sakey")
-
-vim.lsp.enable('pyright')
-
-vim.lsp.config('rust_analyzer', {
-  -- Server-specific settings. See `:help lsp-quickstart`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
-})
--- lua/sakey/init.lua
+++ lua/sakey/init.lua
@@ -3,4 +3,3 @@ vim.g.mapleader = " " -- Set leader key before Lazy
 require("sakey.lazy_init")
 require("sakey.remap")
 require("sakey.set")
-require("sakey.tokyonight")
